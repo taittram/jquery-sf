565ba5225b1f6f76c50cdec0035cf1cddcaa369d
Tai Tram <taittram@yahoo.com>
2015-10-20 23:01:22 -0700

    finished v1

--- js/app.js
+++ js/app.js
@@ -19,21 +19,35 @@ $( document ).ready(function() {
     $("img").mouseup(function() {
     	$("#hadouken").hide();
     	$("#cool").hide();
+    	$("#ready").hide();
     	$("#still").show();
     });
     $("#container").mouseenter(function() {
     	$("#still").hide();
     	$("#ready").show();
+    	$("#cool").hide();
     });
     $("#container").mouseleave(function() {
     	$("#ready").hide();
+    	$("#cool").hide();
     	$("#still").show();
     });
-
+    $("body").keydown(function() {
+    	if (event.which == 88) {
+    	$("#cool").show();
+    	$("#still").hide();
+    	$("#ready").hide();
+    	}
+    });
+    $("body").keyup(function() {
+    	$("#cool").hide();
+    	$("#ready").hide();
+    	$("#still").show();
+    });
 });
 
 function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].volume = 0.2;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
